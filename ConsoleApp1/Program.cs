bf8ea788f997addf9e0edd736e254c8688a7b653
KaliBlip <69836195+KaliBlip@users.noreply.github.com>
2025-01-09 20:56:41 +0000

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Starting of program");
+        Console.WriteLine("");
+
+       Console.Write("Enter your name; ");
+       String name = Console.ReadLine();
+
+        while (name == "")
+        {
+            Console.Write("Enter your name; ");
+            name = Console.ReadLine();
+        }
+       
+       Console.WriteLine("HEllo, " + name );
+       
+        
+        Console.ReadKey();
+    }
+}
